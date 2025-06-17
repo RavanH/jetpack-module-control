2a9a67d72c9f9c2286098e8fd4276b8d38ed3485
RavanH <ravanhagen@gmail.com>
2025-06-17 02:03:28 +0200

    undo precious (no need)

--- jetpack-module-control.php
+++ jetpack-module-control.php
@@ -44,7 +44,10 @@ defined( 'WPINC' ) || die( 'No direct access allowed.' );
 
 define( 'JMC_BASENAME', plugin_basename( __FILE__ ) );
 
-add_action( 'init', array( '\JMC\Plugin', 'init' ) );
+add_filter( 'jetpack_get_default_modules', array( '\JMC\Plugin', 'manual_control' ), 99 );
+add_filter( 'jetpack_offline_mode', array( '\JMC\Plugin', 'development_mode' ) );
+add_filter( 'jetpack_get_available_modules', array( '\JMC\Plugin', 'blacklist' ) );
+
 add_action( 'admin_init', array( '\JMC\Admin', 'init' ), 11 );
 
 register_activation_hook( __FILE__, array( '\JMC\Admin', 'activate' ) );
